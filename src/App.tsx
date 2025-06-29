ce3787f2cb4b6bc86c88a1aa2dc4717dc3f29b0d
Pedro <macpedrot@gmail.com>
2025-06-29 16:06:02 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,74 @@
-import { useActionState, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useActionState } from 'react';
 import './App.css';
-
-const handleLogin = (previousState, formData) => {};
+import { useDispatch } from 'react-redux';
 
 function App() {
-  const [state, formAction] = useActionState((): boolean => true, false);
-  return <></>;
+  const [isLoggedIn, formAction, isPending] = useActionState(
+    handleLogin,
+    false
+  );
+
+  const dispatch = useDispatch();
+
+  async function handleLogin(_: boolean, formData: FormData): Promise<boolean> {
+    try {
+      const response = await fetch(
+        'http://localhost:8080/auth/backoffice/login',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+          body: JSON.stringify({
+            email: formData.get('email'),
+            password: formData.get('password'),
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+      const data = await response.json();
+      console.log('Login successful:', data);
+
+      dispatch({
+        type: 'auth/handleLoginSucceeds',
+        payload: {
+          authToken: data.accessToken,
+          expirationAt: '',
+        },
+      });
+
+      return true;
+    } catch (error) {
+      console.error('Login failed:', error);
+      dispatch({
+        type: 'auth/handleLoginFails',
+      });
+      return false;
+    }
+  }
+
+  return (
+    <>
+      {isPending ? <p>Loading...</p> : null}
+      <form action={formAction}>
+        <label>
+          Email:
+          <input type='text' name='email' required />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type='password' name='password' required />
+        </label>
+        <br />
+        <button type='submit'>Login</button>
+        {isLoggedIn ? <p>Login successful!</p> : null}
+      </form>
+    </>
+  );
 }
 
 export default App;
---
+++ src/redux/auth/slice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+export interface AuthState {
+  authToken: string;
+  expirationAt: string;
+}
+
+const initialState: AuthState = {
+  authToken: '',
+  expirationAt: '',
+};
+
+export const authSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    handleLoginSucceeds: (state, action: PayloadAction<AuthState>) => {
+      state.authToken = action.payload.authToken;
+      state.expirationAt = action.payload.expirationAt;
+    },
+    handleLoginFails: (state) => {
+      state.authToken = '';
+      state.expirationAt = '';
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { handleLoginSucceeds, handleLoginFails } = authSlice.actions;
+
+export default authSlice.reducer;
