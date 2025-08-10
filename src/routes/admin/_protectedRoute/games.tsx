661c673c6cf3766646f0d7f23a42cbfc746f4e01
Pedro <macpedrot@gmail.com>
2025-08-10 10:42:33 +0100

    add dropdown menu for editing and deleting games

--- src/routes/admin/_protectedRoute/games.tsx
+++ src/routes/admin/_protectedRoute/games.tsx
@@ -1,9 +1,19 @@
 import { DataTable } from '@/components/DataTable'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { PageHeader } from '@/components/ui/page-header'
 import { getGames } from '@/services/games/getGames'
 import type { Game, GameListResponse } from '@/services/games/types'
-import { createFileRoute } from '@tanstack/react-router'
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@radix-ui/react-dropdown-menu'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import type { ColumnDef } from '@tanstack/react-table'
+import { MoreHorizontal } from 'lucide-react'
 
 export const Route = createFileRoute('/admin/_protectedRoute/games')({
   component: RouteComponent,
@@ -38,6 +48,33 @@ function RouteComponent() {
     },
     { accessorKey: 'max_players', header: 'Max Players' },
     { accessorKey: 'price', header: 'Price' },
+    {
+      id: 'actions',
+      cell: () => (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="h-8 w-8 p-0 cursor-pointer hover:bg-gray-100 focus-visible:shadow-none focus-visible:outline-none focus-visible: border-none"
+            >
+              {/* <span className="sr-only">Open menu</span> */}
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            className="bg-white shadow-md p-2 rounded"
+          >
+            <DropdownMenuItem className="px-2 py-1 focus:outline-none hover:bg-gray-50 rounded mb-1 cursor-pointer">
+              <Link to="./edit">Edit</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="px-2 py-1 focus:outline-none hover:bg-gray-50 rounded">
+              <Link to="./delete">Delete</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ),
+    },
   ]
 
   return (
