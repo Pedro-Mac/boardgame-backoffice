61c9f7f30957adea65c59cbd307fe08cc8d39a4c
Pedro <macpedrot@gmail.com>
2025-09-10 19:23:19 +0100

    add request with pagination

--- src/services/games/getGames.ts
+++ src/services/games/getGames.ts
@@ -1,8 +1,14 @@
 import { fetchData } from '@/utils/fetchData'
 import type { Game, GameListResponse } from './types'
 
-export const getGames = async (): Promise<GameListResponse> => {
-  const res: { games: Game[] } = await fetchData('/games/', { method: 'GET' })
+export const getGames = async (
+  limit?: number,
+  offset?: number
+): Promise<GameListResponse> => {
+  const res: { games: Game[] } = await fetchData(
+    `/games/?limit=${limit && limit > 0 ? limit : 10}&offset=${offset && offset > 0 ? offset : 0}`,
+    { method: 'GET' }
+  )
   console.log({ res })
   return res
 }
